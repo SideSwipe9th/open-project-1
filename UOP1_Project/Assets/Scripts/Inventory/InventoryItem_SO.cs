0730a7dd6dce0754592bd3fe51babe1033b80dce
Ryan Hamilton <f9thsideswipe@gmail.com>
2020-09-30 17:29:03 +0100

    Add basic InventoryItem Scritpable Object, InventoryItem GameObject script, and InventoryManager for runtime pickup of world items.

---
+++ UOP1_Project/Assets/Scripts/Inventory/InventoryItem_SO.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Assets", menuName ="Game/InventoryItem")]
+public class InventoryItem_SO : ScriptableObject
+{
+    public enum eItemType
+    {
+        Health,
+        Ingredient,
+        Money,
+        Clutter
+    }
+
+    /// <summary>
+    /// The type of the item
+    /// </summary>
+    public eItemType ItemType;
+    /// <summary>
+    /// A reference to the prefab that can be spawned in the world if the player drops the item. 
+    /// </summary>
+    public GameObject gameWorldRuntimePrefab;
+    /// <summary>
+    /// If the item is a consumable like health or money, this value represents the amount the
+    /// player's value of that type should be increased. Eg, if health then heal 10 points. 
+    /// </summary>
+    public int increaseAmount;
+}
---
+++ UOP1_Project/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public List<InventoryItem_SO> backpack = new List<InventoryItem_SO>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // If we've entered the trigger collider for a pickup item
+        // add it to the backpack, and destroy the item in the world
+        // Note: can refactor this to disable if we want to use object pooling
+        if (other.tag == "InventoryItem")
+        {
+            InventoryItem pickupItem;
+            if (other.gameObject.TryGetComponent<InventoryItem>(out pickupItem))
+            {
+                backpack.Add(pickupItem.inventoryItem);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
